6b0475fe53b3f1cd2c42c21ccfc58787b8c72300
dsudo <dsudo0624@gmail.com>
2022-08-15 19:02:09 +0900

    Add sln and projects from templates

---
+++ FAkka.Datasource/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace FAkka.Datasource
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ FAkka.Service/Library.fs
@@ -0,0 +1,5 @@
+﻿namespace FAkka.Service
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
