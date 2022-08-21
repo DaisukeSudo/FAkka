c1422e2be7d4e35fba5c85ab23dc4a12ed62d84b
dsudo <dsudo0624@gmail.com>
2022-08-21 16:16:21 +0900

    hello akka persistence, but failed

--- FAkka.Console/Program.fs
+++ FAkka.Console/Program.fs
@@ -1,7 +1,9 @@
-﻿[<EntryPoint>]
-let main args =
-    FAkka.Datasource.HelloAkka.run ()
+﻿module Program =
 
-    System.Console.ReadLine() |> ignore
-
-    0
+    [<EntryPoint>]
+    let main args =
+        FAkka.Datasource.HelloAkka.run ()
+        FAkka.Datasource.HelloAkka.system.Terminate().Wait()
+        // FAkka.Datasource.HelloAkkaPersistence.run ()
+        // FAkka.Datasource.HelloAkkaPersistence.system.Terminate().Wait()
+        0
--- FAkka.Datasource/HelloAkka.fs
+++ FAkka.Datasource/HelloAkka.fs
@@ -12,3 +12,43 @@ module HelloAkka =
         let helloActor = spawn system "hello" (actorOf hello)
         helloActor <! "Akka.NET F#"
         printfn "end"
+
+
+open Akka.Persistence.FSharp
+
+module HelloAkkaPersistence =
+    let system = System.create "hello-akka-persistence" (Configuration.load ())
+
+    type Command = Put of string | Print
+    type Event = Event of string
+    type State = State of string list
+
+    let update state event =
+        match (state, event) with (State(s), Event(e)) -> State(e :: s)
+        
+    // for OnRecover
+    let apply _ = update
+        
+    // for OnCommand
+    let exec (mailbox: Eventsourced<Command, Event, State>) state cmd =
+        match cmd with
+        | Put s -> mailbox.PersistEvent (update state) [Event(s)]
+        | Print -> match state with State(s) -> String.Join('-', s) |> printfn "State: %s"
+
+    let run () =
+        printfn "start"
+        let actor =
+            spawnPersist
+                system
+                "string-list"
+                {
+                    state = State([])
+                    apply = apply
+                    exec = exec
+                }
+                []
+        actor <! Put "abc"
+        actor <! Print
+        actor <! Put "def"
+        actor <! Print
+        printfn "end"
